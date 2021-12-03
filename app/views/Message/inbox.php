0aba495abab91328d070917b573feeb26111fddf
Christian Kfoury <christian_kfoury558@hotmail.ca>
2021-12-02 22:43:15 -0500

    Inbox messages

--- app/views/Message/inbox.php
+++ app/views/Message/inbox.php
@@ -8,107 +8,157 @@
 <link href="/app/css/style.css" rel="stylesheet">
 
 <head>
-  <title>Your Inbox</title>
+	<title>Your Inbox</title>
 </head>
 
-<body>
-  <nav class="navbar navbar-light bg-light fixed-top">
-    <div class="container-fluid">
-      <a class="navbar-brand mx-auto" href="/User/index" style="margin-left: 50px;"><img src="/uploads/monkey.png" style="height: 100px"></a></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-        <div class="offcanvas-header">
-          <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-          <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body">
-          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-            <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="/User/index">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/Listing/index">Listings</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/Wishlist/index">Wishlist</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/Orders/viewCart">Cart</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" href="/Message/index">Messages</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/User/changePassword">Change Password</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/User/logout">Logout</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="offcanvasNavbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Order history
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="offcanvasNavbarDropdown">
-                <li><a class="dropdown-item" href="/Orders/boughtOrders">Bought order history</a></li>
-                <li>
-                  <hr class="dropdown-divider">
-                </li>
-                <li><a class="dropdown-item" href="/Orders/soldOrders">Sold order history</a></li>
-              </ul>
-            </li>
-          </ul>
-          <form class="d-flex" method="post">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="searchBox">
-            <button class="btn btn-outline-success" type="submit" name="search">Search</button>
-          </form>
-        </div>
-      </div>
-    </div>
-  </nav>
-  <div class="createListing">
-    <center>
-      <h1>Messages</h1>
-    </center>
-    <article>
-      <center>
-        <h1>Inbox</h1>
-        <?php
-        foreach ($data["inbox"] as $messages) {
-          echo
-          "
-                <table border=1>
-                <tr>
-                <th>$messages->sender</th>
-                <th>$messages->message</th>
-                <th>$messages->timestamp</th>
-                <th><a href='\Message\createMessage\\$messages->receiver\\$messages->sender'>Reply</a></th>
-                </tr>
-                </table>
-                ";
-        }
-        ?>
-        <br>
-        <h1>Outbox</h1>
-        <?php
-        foreach ($data["outbox"] as $messages) {
-          echo
-          "
-                <table border=1>
-                <tr>
-                <th>$messages->receiver</th>
-                <th>$messages->message</th>
-                <th>$messages->timestamp</th>
-                <th><a href='\Message\createMessage\\$messages->sender\\$messages->receiver'>Send again</a></th>
-                </tr>
-                </table>
-                ";
-        }
-        ?>
-      </center>
-    </article>
-  </div>
+<body style="background: white;">
+	<nav class="navbar navbar-light bg-light fixed-top">
+		<div class="container-fluid">
+			<a class="navbar-brand mx-auto" href="/User/index" style="margin-left: 50px;"><img src="/uploads/monkey.png" style="height: 100px"></a></a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+				<div class="offcanvas-header">
+					<h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+					<button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+				</div>
+				<div class="offcanvas-body">
+					<ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+						<li class="nav-item">
+							<a class="nav-link" aria-current="page" href="/User/index">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="/Listing/index">Listings</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="/Wishlist/index">Wishlist</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="/Orders/viewCart">Cart</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link active" href="/Message/index">Messages</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="/User/changePassword">Change Password</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="/User/logout">Logout</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="offcanvasNavbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+								Order history
+							</a>
+							<ul class="dropdown-menu" aria-labelledby="offcanvasNavbarDropdown">
+								<li><a class="dropdown-item" href="/Orders/boughtOrders">Bought order history</a></li>
+								<li>
+									<hr class="dropdown-divider">
+								</li>
+								<li><a class="dropdown-item" href="/Orders/soldOrders">Sold order history</a></li>
+							</ul>
+						</li>
+					</ul>
+					<form class="d-flex" method="post">
+						<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="searchBox">
+						<button class="btn btn-outline-success" type="submit" name="search">Search</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</nav>
+	<div class="main">
+		<div style="padding: 20px;">
+			<h1 class="welcome">Messages</h1>
+			<div class="innie">
+				<div class="gray-flex-box">
+					<div class="inbox left-radius">
+						<h2 style="margin-bottom: 20px;">Inbox</h2>
+						<?php
+						foreach ($data["inbox"] as $messages) {
+							echo
+							"
+							<table class='inbox-outbox-table'>
+								<tr>
+									<th>From: $messages->sender</th>
+									<th style='text-align:right; white-space:nowrap;'>At $messages->timestamp</th>
+								</tr>
+								<tr>
+									<td>$messages->message Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis suscipit eaque?</td>
+									<td style='text-align:right'><div class='reply-div'><a class='reply-button' href='\Message\createMessage\\$messages->receiver\\$messages->sender'>Reply</a></div></td>
+								</tr>
+							</table>
+							";
+						}
+						?>
+					</div>
+					<div class="outbox right-radius">
+						<h2 style="margin-bottom: 20px">Outbox</h2>
+						<?php
+						foreach ($data["outbox"] as $messages) {
+							echo
+							"
+							<table class='inbox-outbox-table'>
+								<tr>
+									<th>To: $messages->receiver</th>
+									<th style='text-align:right; white-space:nowrap;'>At $messages->timestamp</th>
+								</tr>
+								<tr>
+									<td>$messages->message</td>
+									<td style='text-align:right'><div class='reply-div'><a class='reply-button' href='\Message\createMessage\\$messages->sender\\$messages->receiver'>Send again</a></div></td>
+								</tr>
+							</table>
+							";
+						}
+						?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- <div class="createListing">
+		<center>
+			<h1>Messages</h1>
+		</center>
+		<article>
+			<center>
+				<h1>Inbox</h1>
+				<?php
+				// foreach ($data["inbox"] as $messages) {
+				// 	echo
+				// 	"
+				// <table border=1>
+				// <tr>
+				// <th>$messages->sender</th>
+				// <th>$messages->message</th>
+				// <th>$messages->timestamp</th>
+				// <th><a href='\Message\createMessage\\$messages->receiver\\$messages->sender'>Reply</a></th>
+				// </tr>
+				// </table>
+				// ";
+				// }
+				?>
+				<br>
+				<h1>Outbox</h1>
+				<?php
+				// foreach ($data["outbox"] as $messages) {
+				// 	echo
+				// 	"
+				// <table border=1>
+				// <tr>
+				// <th>$messages->receiver</th>
+				// <th>$messages->message</th>
+				// <th>$messages->timestamp</th>
+				// <th><a href='\Message\createMessage\\$messages->sender\\$messages->receiver'>Send again</a></th>
+				// </tr>
+				// </table>
+				// ";
+				// }
+				?>
+			</center>
+		</article>
+	</div> -->
 
 </body>
 
